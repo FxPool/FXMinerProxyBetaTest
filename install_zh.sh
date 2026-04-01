a5ff1bfdc01d7578308442afa33e3b1f8ae467bb
FxPool <111183867+FxPool@users.noreply.github.com>
2026-04-01 22:56:41 +0800

    Update install_zh.sh

--- install_zh.sh
+++ install_zh.sh
@@ -1,5 +1,5 @@
 #bin
-version='v15.8.0@260401'
+version='v15.8.1@260401'
 # wget https://github.com/FxPool/FXMinerProxyBetaTest/archive/refs/tags/v15.7.5@260310.tar.gz
 # 转换版本格式
 convert_version() {
